e901d429dba6b84cb99b4171c19d66c20712d66a
Julia Jiang <yiwei.jiang822@icloud.com>
2020-10-25 22:18:51 -0500

    more monads...

--- 12-monads-and-more/exercises.hs
+++ 12-monads-and-more/exercises.hs
@@ -9,8 +9,8 @@ instance Functor Tree where
 {- Create a newtype Ntype that wraps the partially-applied
  - function type to avoid duplicate definition with GHC.Base -}
 data Ntype a b = N (a -> b)
-app :: (Ntype a b) -> a -> b
-app (N f) = f
+appN :: (Ntype a b) -> a -> b
+appN (N f) = f
 
 instance Functor (Ntype a) where
   -- fmap :: (b -> c) -> N (a -> b) -> N (a -> c)
@@ -25,7 +25,7 @@ instance Applicative (Ntype a) where
 
 instance Monad (Ntype a) where
   -- (>>=) :: N (a -> b) -> (b -> N (a -> c)) -> N (a -> c)
-  N f >>= g = N (\x -> app (g (f x)) x)
+  N f >>= g = N (\x -> appN (g (f x)) x)
 
 newtype ZipList a = Z [a] deriving Show
 
@@ -46,5 +46,44 @@ data Expr a = Var a | Val Int | Add (Expr a) (Expr a)
 instance Functor Expr where
   -- fmap :: (a -> b) -> Expr a -> Expr b
   fmap f (Var x) = Var (f x)
-  fmap f (Val n) = Val n
+  fmap _ (Val n) = Val n
   fmap f (Add x y) = Add (fmap f x) (fmap f y)
+
+instance Applicative Expr where
+  -- pure :: a -> Expr a
+  pure x = Var x
+
+  -- <*> :: Expr (a -> b) -> Expr a -> Expr b
+  Var f <*> x = fmap f x
+
+instance Monad Expr where
+  -- (>>=) :: Expr a -> (a -> Expr b) -> Expr b
+  Var x >>= f = f x
+  Val n >>= _ = Val n
+  Add x y >>= f = Add (x >>= f) (y >>= f)
+
+type State = Int
+newtype ST a = S (State -> (a, State))
+
+app :: ST a -> State -> (a, State)
+app (S st) x = st x
+
+instance Functor ST where
+  -- fmap :: (a -> b) -> ST a -> ST b
+  fmap f st = do x <- st
+                 return (f x)
+
+instance Applicative ST where
+  -- pure :: a -> ST a
+  pure x = S (\s -> (x, s))
+
+  -- <*> :: ST (a -> b) -> ST a -> ST b
+  stf <*> stx = do f <- stf
+                   x <- stx
+                   return (f x)
+
+instance Monad ST where
+  -- (>>=) :: ST a -> (a -> ST b) -> ST b
+  st >>= f = S (\s -> let (x, s') = app st s in app (f x) s')
+
+
